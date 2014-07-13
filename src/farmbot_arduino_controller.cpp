2648590b50c3de203b84d077cb0e879f14c23f76
TimEvWw <tim.evers@gmail.com>
2014-07-13 16:26:29 -0100

    End stop pins to input

--- src/farmbot_arduino_controller.cpp
+++ src/farmbot_arduino_controller.cpp
@@ -8,23 +8,23 @@ static GCodeProcessor* gCodeProcessor = new GCodeProcessor();
 
 //The setup function is called once at startup of the sketch
 void setup() {
-	pinMode(X_STEP_PIN, OUTPUT);
-	pinMode(X_DIR_PIN, OUTPUT);
+	pinMode(X_STEP_PIN  , OUTPUT);
+	pinMode(X_DIR_PIN   , OUTPUT);
 	pinMode(X_ENABLE_PIN, OUTPUT);
-	pinMode(X_MIN_PIN, OUTPUT);
-	pinMode(X_MAX_PIN, OUTPUT);
+	pinMode(X_MIN_PIN   , INPUT );
+	pinMode(X_MAX_PIN   , INPUT );
 
-	pinMode(Y_STEP_PIN, OUTPUT);
-	pinMode(Y_DIR_PIN, OUTPUT);
+	pinMode(Y_STEP_PIN  , OUTPUT);
+	pinMode(Y_DIR_PIN   , OUTPUT);
 	pinMode(Y_ENABLE_PIN, OUTPUT);
-	pinMode(Y_MIN_PIN, OUTPUT);
-	pinMode(Y_MAX_PIN, OUTPUT);
+	pinMode(Y_MIN_PIN   , INPUT );
+	pinMode(Y_MAX_PIN   , INPUT );
 
-	pinMode(Z_STEP_PIN, OUTPUT);
-	pinMode(Z_DIR_PIN, OUTPUT);
+	pinMode(Z_STEP_PIN  , OUTPUT);
+	pinMode(Z_DIR_PIN   , OUTPUT);
 	pinMode(Z_ENABLE_PIN, OUTPUT);
-	pinMode(Z_MIN_PIN, OUTPUT);
-	pinMode(Z_MAX_PIN, OUTPUT);
+	pinMode(Z_MIN_PIN   , INPUT );
+	pinMode(Z_MAX_PIN   , INPUT );
 
 	Serial.begin(115200);
 }
