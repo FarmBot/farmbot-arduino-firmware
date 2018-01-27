31f6247a05867cc58fc84ba566dacd763fe784e5
Gabriel Burnworth <gabrielburnworth@users.noreply.github.com>
2018-01-27 09:42:04 -0800

    bump version (v6.0.1)

--- src/Config.h
+++ src/Config.h
@@ -199,9 +199,9 @@
 #endif /* CONFIG_H_ */
 
 #if defined(RAMPS_V14) && !defined(SOFTWARE_VERSION)
-#define SOFTWARE_VERSION "6.0.0.R\0"
+#define SOFTWARE_VERSION "6.0.1.R\0"
 #endif
 
 #if defined(FARMDUINO_V10) && !defined(SOFTWARE_VERSION)
-#define SOFTWARE_VERSION "6.0.0.F\0"
+#define SOFTWARE_VERSION "6.0.1.F\0"
 #endif
