8d979a706aeb125be1dc2c7864b75d5711ff09de
connor rigby <konnorrigby@gmail.com>
2018-05-31 16:37:57 -0700

    Add makefile to project.

--- src/Board.h
+++ src/Board.h
@@ -1,3 +1,21 @@
-//#define RAMPS_V14
-//#define FARMDUINO_V10
-#define FARMDUINO_V14
+#ifndef FARMBOT_BOARD_ID
+
+  //#define RAMPS_V14
+  //#define FARMDUINO_V10
+  #define FARMDUINO_V14
+
+#else
+
+  #undef RAMPS_V14
+  #undef FARMDUINO_V10
+  #undef FARMDUINO_V14
+
+  #if FARMBOT_BOARD_ID == 0
+    #define RAMPS_V14
+  #elif FARMBOT_BOARD_ID == 1
+    #define FARMDUINO_V10
+  #elif FARMBOT_BOARD_ID == 2
+    #define FARMDUINO_V14
+  #endif
+
+#endif
