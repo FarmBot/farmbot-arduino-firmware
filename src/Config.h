a415bf0654dd91398b463d20d2a51c4d59354be0
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2020-01-27 12:59:50 -0800

    bump version (6.5.0)

--- src/Config.h
+++ src/Config.h
@@ -241,17 +241,17 @@ enum MdlSpiEncoders
 #endif /* CONFIG_H_ */
 
 #if defined(RAMPS_V14) && !defined(SOFTWARE_VERSION)
-#define SOFTWARE_VERSION "6.4.2.R\0"
+#define SOFTWARE_VERSION "6.5.0.R\0"
 #endif
 
 #if defined(FARMDUINO_V10) && !defined(SOFTWARE_VERSION)
-#define SOFTWARE_VERSION "6.4.2.F\0"
+#define SOFTWARE_VERSION "6.5.0.F\0"
 #endif
 
 #if defined(FARMDUINO_V14) && !defined(SOFTWARE_VERSION)
-#define SOFTWARE_VERSION "6.4.2.G\0"
+#define SOFTWARE_VERSION "6.5.0.G\0"
 #endif
 
 #if defined(FARMDUINO_EXP_V20) && !defined(SOFTWARE_VERSION)
-#define SOFTWARE_VERSION "6.4.2.E\0"
+#define SOFTWARE_VERSION "6.5.0.E\0"
 #endif
