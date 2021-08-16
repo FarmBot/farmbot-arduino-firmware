730b94dc72c3f19d898b17f67c010fb85ceea672
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2021-08-16 16:14:45 -0700

    fix tmc init issue

--- src/TMC2130_Basics.cpp
+++ src/TMC2130_Basics.cpp
@@ -17,18 +17,18 @@ void TMC2130_Basics::init() {
 }
 
 // initialize SPI
- void TMC2130_Basics::init_SPI() {
-   SPI.setDataMode(FB_TMC_SPI_DATA_MODE);
-   SPI.setBitOrder(FB_TMC_SPI_BIT_ORDER);
-   //SPI.setClockDivider(FB_TMC_SPI_CLOCK_DIVIDER);/**
-   SPI.setClockDivider(SPI_CLOCK_DIV4);
-   SPI.begin();
- }
+void TMC2130_Basics::init_SPI() {
+  SPI.setDataMode(FB_TMC_SPI_DATA_MODE);
+  SPI.setBitOrder(FB_TMC_SPI_BIT_ORDER);
+  //SPI.setClockDivider(FB_TMC_SPI_CLOCK_DIVIDER);/**
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.begin();
+}
 
 // read status
 uint8_t TMC2130_Basics::read_STAT()
 {
-  init_SPI();
+  //init_SPI();
   digitalWrite(_csPin, LOW);
 
   // read address
@@ -47,7 +47,7 @@ uint8_t TMC2130_Basics::read_STAT()
 // read a register
 uint8_t TMC2130_Basics::read_REG(uint8_t address, uint32_t *data)
 {
-   init_SPI();
+  init_SPI();
   digitalWrite(_csPin, LOW);
 
   // read address
